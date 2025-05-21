37b015ef1e803ca6183e8d5aef70bb7d304bec39
Anisa Su <anisa.su@samsung.com>
2025-05-21 22:43:16 +0000

    topo.py: Add file to track topologies
    Used in next commit to automate VM startup/shutdown
    
    Signed-off-by: Anisa Su <anisa.su@samsung.com>

---
+++ topo.py
@@ -0,0 +1,27 @@
+# 1 direct-attached T3 device
+# NO MCTP
+DIRECT_T3 = "-object memory-backend-file,id=cxl-mem1,share=on,mem-path=/tmp/cxltest.raw,size=512M \
+     -object memory-backend-file,id=cxl-lsa1,share=on,mem-path=/tmp/lsa.raw,size=1M \
+     -device pxb-cxl,bus_nr=12,bus=pcie.0,id=cxl.1,hdm_for_passthrough=true \
+     -device cxl-rp,port=0,bus=cxl.1,id=root_port13,chassis=0,slot=2 \
+     -device cxl-type3,bus=root_port13,memdev=cxl-mem1,lsa=cxl-lsa1,id=cxl-pmem0,sn=0xabcd \
+     -M cxl-fmw.0.targets.0=cxl.1,cxl-fmw.0.size=4G,cxl-fmw.0.interleave-granularity=8k"
+
+# 1 DCD with 2 DC regions direct attached to the host
+# and with 1 i2c bus for MCTP
+FM_DCD = "-object memory-backend-file,id=cxl-mem1,mem-path=/tmp/t3_cxl1.raw,size=4G \
+ -device pxb-cxl,bus_nr=12,bus=pcie.0,id=cxl.1,hdm_for_passthrough=true \
+ -device cxl-rp,port=0,bus=cxl.1,id=cxl_rp_port0,chassis=0,slot=2 \
+ -device cxl-type3,bus=cxl_rp_port0,volatile-dc-memdev=cxl-mem1,id=cxl-dcd0,num-dc-regions=2,sn=99 \
+ -machine cxl-fmw.0.targets.0=cxl.1,cxl-fmw.0.size=4G,cxl-fmw.0.interleave-granularity=1k \
+ -device i2c_mctp_cxl,bus=aspeed.i2c.bus.0,address=4,target=cxl-dcd0"
+
+MCTP_SWITCH = ""
+
+MHSLD = ""
+
+TOPOLOGIES = {
+    "DIRECT_T3" : {"topo_str" : DIRECT_T3, "has_mctp" : False},
+    "FM_DCD" : {"topo_str" : FM_DCD, "has_mctp" : True},
+    # "MCTP_SWITCH" : {"topo_str" : MCTP_SWITCH, "has_mctp" : True}
+}
